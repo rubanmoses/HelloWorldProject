498f2e511e56f55b80228e22a488fb0101e571b8
rubanMoses <mosesgeorgeruban@gmail.com>
2019-06-05 19:58:18 +0530

    Ruban Added Hello Good day

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World This is Ruban Moses..!!");
+		System.out.println("Today is a good day!");
+
 	}
 
 }
